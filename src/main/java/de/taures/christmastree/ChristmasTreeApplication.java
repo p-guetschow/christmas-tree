b9cbe96c49fab92f948c8d3b480a3e594df7c4fd
Paavo Gütschow <p_guetschow@taures.de>
2022-10-17 10:07:58 +0200

    initialize

---
+++ src/main/java/de/taures/christmastree/ChristmasTreeApplication.java
@@ -0,0 +1,13 @@
+package de.taures.christmastree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChristmasTreeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ChristmasTreeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/de/taures/christmastree/ChristmasTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.taures.christmastree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChristmasTreeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
